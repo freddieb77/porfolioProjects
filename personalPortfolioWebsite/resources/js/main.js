cc051ebea84d35ffdbcd91e915bee15d8df322bb
Fred Barrett <fred.barrett+github@gmail.com>
2023-03-28 20:34:04 -0500

    Added Read-more link to about, display none for more-info, added JavaScript to change display of more-info to block on mouse click

--- personalPortfolioWebsite/resources/js/main.js
+++ personalPortfolioWebsite/resources/js/main.js
@@ -1,13 +1,9 @@
-const timeContainer = document.getElementById("time-container");
-const BIRTH_DAY = "07-01-2022";
-const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-const intlNumberFormatter = new Intl.NumberFormat("en-US");
+let readMore = document.getElementById('read-more');
+let moreInfo = document.getElementById('more-info');
 
-setInterval(() => {
-  const now = new Date();
-  const difference = Math.floor(
-    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-  );
+// Write your code here:
+const showInfo = () => {
+  moreInfo.style.display = 'block';
+}
 
-  timeContainer.innerText = intlNumberFormatter.format(difference);
-}, 1000);
+readMore.addEventListener('click', showInfo);
